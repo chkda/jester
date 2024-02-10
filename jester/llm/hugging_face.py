55e8fe7ac040dbebc634ad6f41280a022df7ae00
Chhaya <chayakudas.das56@gmail.com>
2024-02-10 21:33:31 +0530

    Minor fix to HF engine

--- jester/llm/hugging_face.py
+++ jester/llm/hugging_face.py
@@ -17,17 +17,19 @@ class HFEngine:
     def stream_response(self, prompt: str) -> StreamingResponse:
         streamer = TextIteratorStreamer(self.tokenizer, timeout=0, 
                     skip_special_tokens=True, skip_prompt=True)
+        self.loop.run_in_executor(None, self.generate_text, prompt, streamer)
         response = StreamingResponse(self.consume_stream(streamer), media_type="text/plain")
         return response
 
     def generate_text(self, prompt: str, streamer: TextIteratorStreamer):
         input_ids = self.tokenizer([prompt], return_tensors="pt").input_ids
-        self.model.generate(input_ids, streamer=streamer, max_length=100)
+        self.model.generate(input_ids, streamer=streamer, max_length=10000, pad_token_id=self.tokenizer.eos_token_id)
 
     async def consume_stream(self, streamer: TextIteratorStreamer):
         while True:
             try:
                 for token in streamer:
+                    print("Toks:", token)
                     yield token
                 break
             except Empty:
